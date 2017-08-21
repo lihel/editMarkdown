3e6f89bfec8abf4e70e3582a66a18c433d826ea9
Ubuntu <835943102@qq.com>
2017-08-21 20:17:23 +0800

    css

--- public/js/components/edit.js
+++ public/js/components/edit.js
@@ -5,11 +5,17 @@ import React from 'react';
 import marked from 'marked';
 import '../../css/edit.css';
 
+
+function Itemlist(props) {
+
+    return <li><a href="#">{props.value}</a></li>
+}
+
 export default class Edit extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            title:document.getElementsByClassName('editTitle').value,
+            title: document.getElementsByClassName('editTitle').value,
             text: document.getElementsByClassName('editText').value
         }
     }
@@ -35,9 +41,25 @@ export default class Edit extends React.Component {
     }
 
     render() {
+        const Items = [1, 2, 3];
         return (
             <div>
-                <button onClick={this.handleClick.bind(this)}>发布</button>
+                <div className="Head">
+                    <img src="../../images/photo.jpeg" width={60} height={50}/>
+                    用户名
+                    <button onClick={this.handleClick.bind(this)}>发布</button>
+                </div>
+                <div className="articlelist">
+                    <p>我的文章</p>
+                    <ul>
+                        {Items.map((val)=>
+                            <Itemlist key={val.toString()}
+                                      value={val}/>
+                        )}
+                    </ul>
+                </div>
+
+
                 <div className="editArea">
                     <textarea className="editTitle" onChange={this.handelTitleChange.bind(this)} ref="title"/>
 
